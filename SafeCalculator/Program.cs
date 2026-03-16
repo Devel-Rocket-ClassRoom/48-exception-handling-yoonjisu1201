77fccd0ddae715d05a19dfe1b8ce3665194c958f
yoonjisu1201 <yoonjisu1201@gmail.com>
2026-03-16 18:00:10 +0900

    Update Program.cs

--- SafeCalculator/Program.cs
+++ SafeCalculator/Program.cs
@@ -1,4 +1,15 @@
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+
+public class SafeCalculator
+{
+    public void Divide(string num1, string num2)
+    {
+        if (true)
+        {
+            
+        }
+    }
+}
